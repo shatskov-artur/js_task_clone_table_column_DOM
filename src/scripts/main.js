35eddbd49f35b140b0f478cbf802c40b7898927c
Artur Shatskov <artur.shackov77@gmail.com>
2025-07-01 23:53:44 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// write your code here
+const thead = document.querySelector('table thead tr');
+const tbody = document.querySelector('table tbody');
+const tfoot = document.querySelector('table tfoot tr');
+
+[thead, tfoot].forEach((element) => {
+  Array.from(element.children).forEach((child, index) => {
+    if (index === 1) {
+      const copy = document.createElement('th');
+
+      copy.textContent = child.textContent;
+
+      element.insertBefore(copy, element.lastElementChild);
+    }
+  });
+});
+
+Array.from(tbody.children).forEach((element) => {
+  Array.from(element.children).forEach((child, index) => {
+    if (index === 1) {
+      const copy = document.createElement('td');
+
+      copy.textContent = child.textContent;
+
+      element.insertBefore(copy, element.lastElementChild);
+    }
+  });
+});
